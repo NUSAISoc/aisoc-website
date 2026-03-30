fa99108a1a676959b69afff630ab42e881eaf7cb
itsvari <13640661+itsvari@users.noreply.github.com>
2026-03-31 04:05:40 +0800

    feat: modify content collection schemas
    - allow events and blogposts to have ISO8601 date/datetimes with timezones
    - make image optional for exco members
    - add past exco member field

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,8 +9,8 @@ const eventsCollection = defineCollection({
   }),
   schema: z.object({
     title: z.string(),
-    startDate: z.string().datetime().or(z.date()),
-    endDate: z.string().datetime().or(z.date()),
+    startDate: z.iso.datetime({ offset: true }).or(z.date()),
+    endDate: z.iso.datetime({ offset: true }).or(z.date()),
     location: z.string(),
     description: z.string().optional(),
     image: z.string().optional(),
@@ -27,8 +27,9 @@ const teamCollection = defineCollection({
     name: z.string(),
     role: z.string(),
     tagline: z.string().max(100),
-    image: z.string(),
+    image: z.string().optional(),
     order: z.number(),
+    isPastMember: z.boolean().optional().default(false),
     social: z
       .object({
         github: z.url().optional(),
@@ -49,7 +50,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     author: z.string(),
-    date: z.string().datetime().or(z.date()),
+    date: z.iso.datetime({ offset: true }).or(z.date()),
     excerpt: z.string(),
     tags: z.array(z.string()),
     coverImage: z.string().optional(),
